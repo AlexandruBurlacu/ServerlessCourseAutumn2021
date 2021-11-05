ac18987d17e266e88422b8b1008b5131374f72da
Alex Burlacu <alexburlacu1996@gmail.com>
2021-11-05 03:24:49 +0200

    a bit cleaner, and delete works

--- lesson_3/app.py
+++ lesson_3/app.py
@@ -3,19 +3,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from collections import defaultdict
+from functools import partial
 
 import docker
 
 
-code_repo_path = "code_repo/"
-functions_db = defaultdict(list) # gospadi prasti
-# EventType: List[FunctionDefinition]
-# EventType example "http|GET|/api/cats" it's a complex key
-
-
-app = FastAPI()
-
-EventType = str # Enum
+EventType = str # EventType example "http|GET|/api/cats" it's a complex key
 Code = str
 
 class FunctionDefinition(BaseModel):
@@ -24,47 +17,13 @@ class FunctionDefinition(BaseModel):
     function_name: str
     # function_owner
 
-
-
-
-@app.get("/functions")
-def list_available_functions():
-    global functions_db
-    return functions_db # TODO: remove function_code path from response
-
-
-def handle_code(function_key, function_code, function_name):
-    code_type = function_key.replace('|', "__").replace("/", "---") # http|GET|/api/cats -> http__GET__---api---cats
-    function_code_dir = f"{os.path.join(code_repo_path, code_type)}"
-
-    os.makedirs(function_code_dir, exist_ok=True)
-
-    function_code_path = os.path.join(function_code_dir, f"{function_name}.py")
-    with open(function_code_path, "w") as fptr:
-        fptr.write(function_code)
-
-    return function_code_path
-
-
-@app.post("/functions")
-def create_function(func_def: FunctionDefinition):
-    global functions_db
-    key = func_def.event_type
-
-    function_code_path = handle_code(key, func_def.function_code, func_def.function_name)
-    
-    func_def_dict = func_def.dict()
-    func_def_dict["function_code"] = function_code_path
-
-    functions_db[key].append(func_def_dict)
-    return {"status": "ok"}
-
-
-@app.delete("/functions/:function_name") # TODO
-def delete_function(function_name: str):
-    # for event_t, func_defs in functions_db.items():
-        
-    return {"status": "ok"}
+    @staticmethod
+    def example():
+        return {
+                "event_type": "http|GET|/api/cats",
+                "function_code": "import this",
+                "function_name": "calling_cats_not"
+                }
 
 
 Header = str
@@ -78,27 +37,104 @@ class HttpEventPayload(BaseModel):
     http_path: Path
     http_verb: Verb
 
+    @staticmethod
+    def example():
+        return {
+                "http_header": "Content-Type: text/plain\nX-Special-Header: i-think-they-are-deprecated",
+                "http_body": "See also: https://stackoverflow.com/questions/3561381/custom-http-headers-naming-conventions, anyway, show us cats!",
+                "http_path": "/api/cats",
+                "http_verb": "GET"
+                }
+
+
+
+CODE_REPO_PATH = "code_repo/"
+FUNCTIONS_DB = defaultdict(list) # gospadi prasti
+# EventType: List[FunctionDefinition]
+# EventType example "http|GET|/api/cats" it's a complex key
+
+
+app = FastAPI()
+
+
+
+def _sanitize_function_def(func_def: dict, drop_fields: list):
+    clean_func_def = func_def.copy()
+    for field in drop_fields:
+        if field in func_def:
+            del clean_func_def[field]
+    return clean_func_def
+
+
+def sanitize(function_defs: list[dict]):
+    return list(map(partial(_sanitize_function_def, drop_fields=["function_code"]), function_defs))
+
+
+@app.get("/functions")
+def list_available_functions():
+    global FUNCTIONS_DB
+    response = {event_t: sanitize(function_defs) for event_t, function_defs in FUNCTIONS_DB.items()}
+    return response
+
+
+def handle_code(function_key, function_code, function_name):
+    code_type = function_key.replace('|', "__").replace("/", "---") # http|GET|/api/cats -> http__GET__---api---cats
+    function_code_dir = f"{os.path.join(CODE_REPO_PATH, code_type)}"
+
+    os.makedirs(function_code_dir, exist_ok=True)
+
+    function_code_path = os.path.join(function_code_dir, f"{function_name}.py")
+    with open(function_code_path, "w") as fptr:
+        fptr.write(function_code)
+
+    return function_code_path
+
+
+@app.post("/functions")
+def create_function(func_def: FunctionDefinition = FunctionDefinition.example()):
+    global FUNCTIONS_DB
+    key = func_def.event_type
+    
+    func_def_dict = func_def.dict()
+    func_def_dict["function_code"] = handle_code(key, func_def.function_code, func_def.function_name)
+
+    FUNCTIONS_DB[key].append(func_def_dict)
+    return {"status": "ok"}
+
+
+@app.delete("/functions/{function_name}")
+def delete_function(function_name: str = "calling_cats_not"):
+    for event_t, func_defs in FUNCTIONS_DB.items():
+        for idx, func_def in enumerate(func_defs):
+            if func_def["function_name"] == function_name:
+                del FUNCTIONS_DB[event_t][idx]
+        
+    return {"status": "ok"}
+
+
 
 def make_key_from_payload(payload: dict):
     return f"http|{payload.get('http_verb')}|{payload.get('http_path')}"
 
 
 @app.post("/functions/trigger")
-def trigger_functions_execution(payload: HttpEventPayload):
-    global functions_db
+def trigger_functions_execution(payload: HttpEventPayload = HttpEventPayload.example()):
+    global FUNCTIONS_DB
     controller = docker.from_env()
 
     key = make_key_from_payload(payload.dict())
 
     all_logs = []
-    for function in functions_db.get(key, []):
-        f_name = function.get("function_name")
+    for function in FUNCTIONS_DB.get(key, []):
         f_code_path = function.get("function_code")
 
-        logs = controller.containers.run("python:3.8-slim", f"python {os.path.join('/tmp/', f_code_path)}",
-                                        volumes={os.path.join(os.getcwd(), code_repo_path): {
-                                                        "bind": os.path.join("/tmp/", code_repo_path),
-                                                        "mode": "ro"}})
+        volumes = {os.path.join(os.getcwd(), CODE_REPO_PATH): {
+                            "bind": os.path.join("/tmp/", CODE_REPO_PATH),
+                            "mode": "ro"}}
+
+        command = f"python {os.path.join('/tmp/', f_code_path)}"
+
+        logs = controller.containers.run("python:3.8-slim", command, volumes=volumes)
         all_logs.append(logs)
 
     return {"status": "ok", "all_logs": all_logs}
